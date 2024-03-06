294f90030111210accb1936fe2c9e4cc3ec3884f
kavan72 <vadaliya.kavan@gmail.com>
2024-03-06 18:13:37 +0530

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Test, {FilterType} from "@/components/Test";
 import {useQueryStates} from "nuqs";
-import useRenderingTrace from "@/hooks/useRenderingTrace";
+import {useEffect} from "react";
 
 
 export default function Home() {
@@ -11,7 +11,9 @@ export default function Home() {
         clearOnDefault: true,
     })
 
-    useRenderingTrace('Home', { filters, setFilter })
+    useEffect(() => {
+        console.log(filters)
+    }, [filters.query, filters.leadStatus]);
 
     return (
         <Test filters={filters} setFilter={setFilter}/>
